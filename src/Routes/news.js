1e09b40b139949de983458c2e538350a230d6714
unknown <yudiprayogaraharja@gmail.com>
2020-01-29 17:44:14 +0700

    update patch

--- src/Routes/news.js
+++ src/Routes/news.js
@@ -8,5 +8,5 @@ const Router = express();
 
 Router.get('/', controller.getNews);
 Router.post('/', controller.postNews);
-Router.patch('/:id_news', controller.patchNews);
+Router.patch('/edit/:id_news', controller.patchNews);
 module.exports = Router;
